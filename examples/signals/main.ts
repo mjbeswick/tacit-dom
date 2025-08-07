bbf39716904b7d21866c7b18be2bf3537e974d62
Michael Beswick <michael.beswick@tesco.com>
2025-08-07 15:39:14 +0100

    fix: update router import to use Signal type from signals module

--- examples/signals/main.ts
+++ examples/signals/main.ts
@@ -14,13 +14,13 @@ const random = () => Math.random().toString(36).slice(2, 6).toUpperCase();
 const signalA = signal(random());
 const signalB = signal(random());
 
-const signalC = computed(() => {
+const computedA = computed(() => {
   console.log('computed signalC');
   return signalA.get() + ' ' + signalB.get();
 });
 
 effect(() => {
-  console.log('effect signalC', signalC.get());
+  console.log('effect signalA', computedA.get());
 });
 
 const app = () => {
@@ -29,13 +29,25 @@ const app = () => {
     signalB.set(random());
   };
 
+  const signalD = signal(random());
+
+  const computedB = computed(() => {
+    console.log('computed signalB', signalD.get());
+    return signalD.get();
+  });
+
+  effect(() => {
+    console.log('effect signalB', computedB.get());
+  });
+
   return div(
     h1('Domitor Signals Example'),
     div(button({ onclick: handleUpdate }, 'Update signals')),
     div(
       p('signalA: ', signalA),
       p('signalB: ', signalB),
-      p('signalC (computed): ', signalC),
+      p('signalC (computed): ', computedA),
+      p('signalD: ', signalD),
     ),
   );
 };
